a58da25729f88f3cb2fa78b5026a484a6ef9b8bd
Salvatore D'Angelo <sdangelo@it.ibm.com>
2018-12-18 21:59:35 +0100

    Separate data from binaries

--- clean_containers.sh
+++ clean_containers.sh
@@ -3,6 +3,12 @@ NODE1_NAME=node1
 NODE2_NAME=node2
 NODE3_NAME=node3
 
+# Docker volumes
+NODE1_VOLUME=volume1
+NODE2_VOLUME=volume2
+NODE3_VOLUME=volume3
+
+
 # Nodes private network name
 PRIVATE_NETWORK_NAME=node_private_bridge
 
@@ -13,4 +19,7 @@ docker stop ${NODE3_NAME}
 docker rm ${NODE1_NAME}
 docker rm ${NODE2_NAME}
 docker rm ${NODE3_NAME}
+docker volume rm ${NODE1_VOLUME}
+docker volume rm ${NODE2_VOLUME}
+docker volume rm ${NODE3_VOLUME}
 docker network rm ${PRIVATE_NETWORK_NAME}
